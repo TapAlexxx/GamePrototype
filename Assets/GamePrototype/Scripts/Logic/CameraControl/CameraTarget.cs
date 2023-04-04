dac5d0fd6772b17ecf53bc1dffcb5f062f8fad82
TapAlexxx <lordpictures123@gmail.com>
2023-04-04 22:00:01 +0300

    refactored CameraTarget

--- Assets/GamePrototype/Scripts/Logic/CameraControl/CameraTarget.cs
+++ Assets/GamePrototype/Scripts/Logic/CameraControl/CameraTarget.cs
@@ -4,8 +4,8 @@ namespace GamePrototype.Scripts.Logic.CameraControl
 {
     public class CameraTarget : MonoBehaviour
     {
-        [SerializeField] private Transform _carTransform;
-        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Transform _playerTransform;
+        [SerializeField] private Rigidbody _cameraTargetRigidbody;
         [SerializeField] private float _xDamping;
         [SerializeField] private float _yDamping;
         [SerializeField] private float _zDamping;
@@ -15,12 +15,12 @@ namespace GamePrototype.Scripts.Logic.CameraControl
 
         private void FixedUpdate()
         {
-            Vector3 rbPos = _rigidbody.position;
-            rbPos.x = Mathf.Lerp(rbPos.x, _carTransform.position.x, _xDamping * Time.fixedDeltaTime);
-            rbPos.y = Mathf.Lerp(rbPos.y, _carTransform.position.y, _yDamping * Time.fixedDeltaTime);
-            rbPos.z = Mathf.Lerp(rbPos.z, _carTransform.position.z, _zDamping * Time.fixedDeltaTime);
-            _rigidbody.MoveRotation(_carTransform.rotation);
-            _rigidbody.MovePosition(rbPos);
+            Vector3 rbPos = _cameraTargetRigidbody.position;
+            rbPos.x = Mathf.Lerp(rbPos.x, _playerTransform.position.x, _xDamping * Time.fixedDeltaTime);
+            rbPos.y = Mathf.Lerp(rbPos.y, _playerTransform.position.y, _yDamping * Time.fixedDeltaTime);
+            rbPos.z = Mathf.Lerp(rbPos.z, _playerTransform.position.z, _zDamping * Time.fixedDeltaTime);
+            _cameraTargetRigidbody.MoveRotation(_playerTransform.rotation);
+            _cameraTargetRigidbody.MovePosition(rbPos);
         }
     }
 }
