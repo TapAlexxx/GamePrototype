9f9e59d44ba3cec9a7134d1ec0534d51576b65b1
TapAlexxx <lordpictures123@gmail.com>
2023-04-04 22:04:31 +0300

    implement player state control and recoil control

---
+++ Assets/GamePrototype/Scripts/Logic/PlayerControl/PlayerStateControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using GamePrototype.Scripts.Logic.PlayerInputControl;
+using UnityEngine;
+
+namespace GamePrototype.Scripts.Logic.PlayerControl
+{
+    public class PlayerStateControl : MonoBehaviour
+    {
+        [SerializeField] private PlayerDragInput _playerDragInput;
+        [SerializeField] private RecoilControl _recoilControl;
+
+        private void OnValidate()
+        {
+            if (!_playerDragInput) TryGetComponent(out _playerDragInput);
+            if (!_recoilControl) TryGetComponent(out _recoilControl);
+        }
+
+        private void Start()
+        {
+            _recoilControl.MoveEnded += EnterPlayState;
+            _recoilControl.MoveStarted += EnterWaitState;
+        }
+
+        private void OnDestroy()
+        {
+            _recoilControl.MoveEnded -= EnterPlayState;
+            _recoilControl.MoveStarted -= EnterWaitState;
+        }
+
+        public void EnterWaitState()
+        {
+            Debug.Log("WaitState");
+            _playerDragInput.Disable();
+        }
+
+        public void EnterPlayState()
+        {
+            Debug.Log("PlayeState");
+            _playerDragInput.Activate();
+            _recoilControl.Activate();
+        }
+
+        public void EnterFinishState()
+        {
+            _playerDragInput.Disable();
+            _recoilControl.Disable();
+        }
+    }
+}
---
+++ Assets/GamePrototype/Scripts/Logic/PlayerControl/RecoilControl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using GamePrototype.Scripts.Logic.PlayerInputControl;
+using UnityEngine;
+
+namespace GamePrototype.Scripts.Logic.PlayerControl
+{
+    public class RecoilControl : MonoBehaviour
+    {
+        [SerializeField] private PlayerDragInput _playerDragInput;
+        [SerializeField] private Rigidbody _rigidbody;
+        
+        private Coroutine _observeCoroutine;
+        private bool _active;
+        public event Action MoveEnded;
+        public event Action MoveStarted;
+
+        private void OnValidate()
+        {
+            if (!_playerDragInput) TryGetComponent(out _playerDragInput);
+            if (!_rigidbody) TryGetComponent(out _rigidbody);
+        }
+
+        private void Start() => 
+            _playerDragInput.EndDrag += ObserveEndOfMove;
+
+        private void OnDestroy() => 
+            _playerDragInput.EndDrag -= ObserveEndOfMove;
+
+        private void ObserveEndOfMove()
+        {
+            if(!_active)
+                return;
+            MoveStarted?.Invoke();
+            Debug.Log("Started");
+            TryStopCoroutine(ref _observeCoroutine);
+            _observeCoroutine = StartCoroutine(Observe());
+        }
+
+        private void TryStopCoroutine(ref Coroutine coroutine)
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+        }
+
+        private IEnumerator Observe()
+        {
+            yield return new WaitForSeconds(1f);
+            while (_rigidbody.velocity.magnitude > 0)
+            {
+                yield return null;
+            }
+
+            MoveEnded?.Invoke();
+            TryStopCoroutine(ref _observeCoroutine);
+        }
+
+        public void Disable()
+        {
+            TryStopCoroutine(ref _observeCoroutine);
+            _active = false;
+        }
+
+        public void Activate()
+        {
+            _active = true;
+        }
+    }
+}
